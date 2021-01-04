cf87a859d01463db1bb7c1bc79200d589568e602
Miguel <miguelmakio61@gmail.comgit config --global user.email miguelmakio61@gmail.com;git config --global user.email miguelmakio61@gmail.com>
2021-01-04 13:18:03 -0500

    Se crea proyecto modulo de ventas autopro, primer commit

---
+++ autopro-ventas/src/main/java/com/autoproventas/AutoproVentasApplication.java
@@ -0,0 +1,13 @@
+package com.autoproventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoproVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoproVentasApplication.class, args);
+	}
+
+}
