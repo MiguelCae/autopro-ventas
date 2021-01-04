951d7fda2ce45d3ab259722dc8f0718a286a23cb
Miguel <miguelmakio61@gmail.comgit config --global user.email miguelmakio61@gmail.com;git config --global user.email miguelmakio61@gmail.com>
2021-01-04 18:04:47 -0500

    Se levanta servidor

--- ventas-autopro/src/main/java/com/autopro/ventasautopro/HolaMundo.java
+++ ventas-autopro/src/main/java/com/autopro/ventasautopro/HolaMundo.java
@@ -1,4 +1,19 @@
 package com.autopro.ventasautopro;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/saludar")
+
+
 
 public class HolaMundo {
+
+    @GetMapping("/hola")
+
+    public String saludar(){
+        return "Hola mundoooo";
+    }
 }
